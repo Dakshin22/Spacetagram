5d10691b40b86a975ea91a8d2a3e7780072efd17
Dakshin R <drathan45@gmail.com>
2021-09-20 20:52:00 -0400

    remove router - not needed

--- spacetagram/src/App.js
+++ spacetagram/src/App.js
@@ -1,22 +1,17 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./navigation/Navbar";
-import ResultsPage from "./pages/ResultsPage"
+import ResultsPage from "./pages/ResultsPage";
 function App() {
   return (
     <>
-      <Router>
-        <div className="App">
-          <NavBar />
-          <div id="page-body">
-            <Switch>
-              <Route path="/" component={ResultsPage} exact />
-            </Switch>
-          </div>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <ResultsPage />
         </div>
-      </Router>
+      </div>
     </>
   );
 }
